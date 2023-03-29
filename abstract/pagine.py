97a42a5124314c33e784e83ba04db6a00bc2024f
Hugo Italo Magalhães Correia <84601208+HugoItaloMC@users.noreply.github.com>
2023-03-29 13:34:40 -0300

    UPDATE: interface APIMeta

--- abstract/pagine.py
+++ abstract/pagine.py
@@ -1,17 +1,28 @@
 
 from abc import ABCMeta, abstractmethod
 
+from config._2handler import Index
 
-class APIMeta(metaclass=ABCMeta):
+class APIMeta(Index, metaclass=ABCMeta):
+   
     # Interface API
+   
     @abstractmethod
     def _post(self):
-        (...)
+        ...
 
     @abstractmethod
     def _put(self):
-        (...)
+        ...
 
     @abstractmethod
     def _remove(self):
+        ...
+
+    @abstractmethod
+    def _getall(self):
+        ...
+    
+    @abstractmethod
+    def _get(self):
         ...
